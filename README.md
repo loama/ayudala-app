98e6d4b8140193f29f883245dc8bfb2f0f36502e
Jaime Lopez <loama18@gmail.com>
2019-08-24 12:15:58 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ayudala app
+
+## Project creation
+cordova create app com.ayudala.app Ayudala --template cordova-template-framework7-vue-webpack
---
+++ src/main.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>hola</div>
+</template>
+<script>
+// Import Routes...
+import routes from './routes.js'
+
+export default {
+    components: {
+    },
+    data() {
+        return {
+        }
+    }
+}
+</script>
